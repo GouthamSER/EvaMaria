b17b3f90fb419eec5d4151dd660b9ffd1026fd56
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-04-17 19:32:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from Script import script
 from datetime import date, datetime 
 import pytz
 from aiohttp import web
